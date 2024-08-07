b05c2973f9b9994d600d2548425673f4fbf3f2ca
Callahan Stormer <33109142+CallStormer@users.noreply.github.com>
2024-08-07 09:55:10 -0700

    A fix for GTest

--- tests/tHydroTests/tHydroModelTests.cpp
+++ tests/tHydroTests/tHydroModelTests.cpp
@@ -1,6 +1,5 @@
 #include "src/tHydro/tHydroModel.h"
-#include <gtest/gtest.h>
-
+#include "gtest/gtest.h"
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
