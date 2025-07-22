32bf50e80c07f6140b7651956dafee92fd7f59fd
Josh Cederstrom <joshcederstrom@gmail.com>
2025-07-22 12:30:30 -0700

    Fix ComputeVoronoiArea function in meshElements.cpp that was setting the incorrect voronoi area value for the node connected to the outlet node in the mesh.

--- src/tMeshElements/meshElements.cpp
+++ src/tMeshElements/meshElements.cpp
@@ -452,7 +452,8 @@ double tNode::ComputeVoronoiArea()
 			xy1 = ne->getRVtx();
 			//checking polygon edge is on boundary and ccw edge's RVtx is on
 			//wrong side of bndy edge...
-			if( ce->getBoundaryFlag() && ne->getBoundaryFlag() )
+			// CJC 2025 debug if statement wouldn't execute for voronoi connected to outlet point causing area to be equal to last node
+			if( ce->getDestinationPtrNC()->getBoundaryFlag() != kNonBoundary && ne->getDestinationPtrNC()->getBoundaryFlag() != kNonBoundary )
 			{
 				bn0 = ce->getDestinationPtrNC();
 				bn1 = ne->getDestinationPtrNC();
